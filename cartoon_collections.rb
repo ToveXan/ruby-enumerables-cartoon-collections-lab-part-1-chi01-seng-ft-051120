0791b4356749591e58d95af3c4f0dfde743f4d32
Zoe Xanos <zxanos@gmail.com>
2020-05-10 08:40:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |index, value|
-    puts "#{index}: #{value}"
+    puts "#{value}: #{index}"
   end
 end
 dwarves_array = ["Gimli", "Thorin", "Balin", "Gloin"]

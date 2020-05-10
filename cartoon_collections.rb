52ade367fd2139e4c1b76392fcfd942fd26f638a
Zoe Xanos <zxanos@gmail.com>
2020-05-10 08:38:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,7 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  dwarves_array = ["Gimli", "Thorin", "Balin", "Gloin"]
+  
 end
+dwarves_array = ["Gimli", "Thorin", "Balin", "Gloin"]
+list_dwarves(dwarves_array)

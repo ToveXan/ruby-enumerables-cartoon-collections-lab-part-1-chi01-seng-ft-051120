1b592c97c25cc912c20d4b9c974dfd873b28632b
Zoe Xanos <zxanos@gmail.com>
2020-05-10 08:41:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index(1) do |index, value|
+  array.each.with_index(1) do |index, value|
     puts "#{value}: #{index}"
   end
 end
